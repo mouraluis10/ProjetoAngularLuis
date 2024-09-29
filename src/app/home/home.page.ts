df0f094bcf89effee0ff8ec3ecab5a75a3bd46bb
Luis Moura <125521227+mouraluis10@users.noreply.github.com>
2024-09-29 21:22:04 +0000

    pronto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { IonicModule, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public nome: string = "";
+  public sobrenome: string = "";
+  public nomeCompleto: string = "";
 
-  
-  teste(){
-    console.log("teste");
+  constructor(private alertController:AlertController) { }
+
+  async aviso() {
+
+    const mensagem = await this.alertController.create({
+      header: 'Nome completo',
+      message: 'O seu nome completo é ' + this.nomeCompleto,
+      buttons: ["OK"]
+    });
+
+    await mensagem.present();
   }
-}
 
+  concatenarNomes(){
+    this.nomeCompleto = this.nome + " " + this.sobrenome;
+    this.aviso();
+  }
+  
+}
